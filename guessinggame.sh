d134099728df0c8186008d1f2e3e5d7bdf2375b5
Tatz884 <tatsuya884.0201@gmail.com>
2019-07-09 17:46:59 -0600

    wrote the blueprint in the gussinggame.sh for this assignment

---
+++ guessinggame.sh
@@ -0,0 +1,11 @@
+#This is the blue print for making guessinggame.sh
+
+# 1. I need to count the number of files in the current directry by regular expression or find?
+
+# 2. I need to find the command to ask prompt for a guess
+
+# 3. Use while-loop to ensure the requirement
+
+# 4. I need to make if-then statement for checking if the user's inputs are correct or not
+
+# 5. Dunno how to use function in this bash file? need to review function part of the lecture.
